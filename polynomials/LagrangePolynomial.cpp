e167d7443d8c66ef353b8d789d9aa252e83418d7
/rdndev <rdninc@mail.ru>
2022-03-04 21:25:44 +0200

    add an comment

--- polynomials/LagrangePolynomial.cpp
+++ polynomials/LagrangePolynomial.cpp
@@ -5,11 +5,11 @@
 * @param x - точка в которое многочлен будет вычислен
 * @param xv - массив значений точек на оси Ox
 * @param yv - значение в точке xv[i]
-* @return (double) полином Лагранжа в точке x
+* @return (double) значения полинома Лагранжа в точке x
 */
 double LagrangePolynomial(double x, vector<double> xv, vector<double> yv) {
 	int size = xv.size(); // Количество точек, по которым строим многочлен
-	double L = 0.0;
+	double L = 0.0; 
 	for (size_t i = 0; i < size; i++) {
 		double basisPol = 1.0;
 		for (size_t j = 0; j < size; j++) {
